aaf123a9f3341c2030b49547f1b1e03262f8a272
9838dev <maheshwaridev908@gmail.com>
2021-04-08 00:19:02 +0530

    database created

--- chatBackend/app/admin.py
+++ chatBackend/app/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=[
+    'id','name','email','password','date','phone_no'
+    ]
+
+class FriendAdmin(admin.ModelAdmin):
+    list_display = [
+    'id','user1_id','user2_id','status','date'
+    ]
+
+class friend_messageAdmin(admin.ModelAdmin):
+    list_display=[
+    'id','friend_id','sender_id','date','message'
+    ]
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display=[
+    'id','creater_id','name','title','created_date'
+    ]
+
+class ParticipantsAdmin(admin.ModelAdmin):
+    list_display = [
+    'id','group_id','participant_id'
+    ]
+
+class group_messageAdmin(admin.ModelAdmin):
+    list_display = [
+    'id','group_id','sender_id','message','date'
+    ]
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Friend,FriendAdmin)
+admin.site.register(friend_message,friend_messageAdmin)
+admin.site.register(Group,GroupAdmin)
+admin.site.register(Participants,ParticipantsAdmin)
+admin.site.register(group_message,group_messageAdmin)
